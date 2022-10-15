bdd6f0661e9f47946b313a123fb858e38d4799cc
Илюшенька Антонов <igor.flam2017@gmail.com>
2022-10-15 16:18:23 +0300

    Added project solution (Console App)

---
+++ Mini_Project_Assemble_Pizza/Mini_Project_Assemble_Pizza/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mini_Project_Assemble_Pizza
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
