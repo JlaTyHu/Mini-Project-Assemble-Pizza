f0ee1dabdbb00984a4c329c134d28cd53abfb8d7
DaraGinger <darinafrouk@gmail.com>
2022-10-16 11:47:42 +0300

    Game entry

---
+++ Mini_Project_Assemble_Pizza/Mini_Project_Assemble_Pizza/Lobby/Lobby.cs
@@ -0,0 +1,49 @@
+﻿namespace Mini_Project_Assemble_Pizza.Lobby
+{
+    using System;
+    public class Lobby
+    {
+        public void Registration()
+        {
+            Console.WriteLine("Registration");
+
+            Console.WriteLine("Enter your name:");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Enter your age:");
+
+            int ageUser = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(new string('-', 50));
+        }
+
+        public void BeginOfGame()
+        {
+            Console.WriteLine("Lobby");
+
+            bool error = true;
+
+            while (error)
+            {
+                Console.WriteLine("Begin game (Press y) \t Leadboeard(Press t)");
+
+                char userChoice = Char.Parse(Console.ReadLine());
+
+                switch (userChoice)
+                {
+                    case 'y': 
+                        error = false; 
+                        break;
+                    case 't': 
+                        error = false; 
+                        break;
+                    default: 
+                        error = true; 
+                        Console.WriteLine("No such option exist"); 
+                        break;
+                }
+            }
+        }
+    }
+}
