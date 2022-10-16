e57aff6beb672da7e84c7ed1cf9ea06447ffbc4e
Илюшенька Антонов <igor.flam2017@gmail.com>
2022-10-16 12:16:17 +0300

    Added display collection

---
+++ Mini_Project_Assemble_Pizza/Mini_Project_Assemble_Pizza/Collection/OperationOfCollection.cs
@@ -0,0 +1,37 @@
+﻿namespace Mini_Project_Assemble_Pizza.Collection
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class OperationOfCollection
+    {
+        public List<string> FillCollection(List<string> ingredientsForPizza)
+        {
+            ingredientsForPizza.Add("Cheese");
+            ingredientsForPizza.Add("Tomato");
+            ingredientsForPizza.Add("Mushrooms");
+            ingredientsForPizza.Add("Sausage");
+            ingredientsForPizza.Add("Meat");
+            ingredientsForPizza.Add("Pineapple");
+            ingredientsForPizza.Add("Mayonnaise");
+            ingredientsForPizza.Add("Ketchup");
+            ingredientsForPizza.Add("Bacon");
+            ingredientsForPizza.Add("Onion");
+            ingredientsForPizza.Add("Olives");
+            ingredientsForPizza.Add("Pepper");
+
+            return ingredientsForPizza;
+        }
+
+        public void Display(List<string> ingredientsForPizza)
+        {
+            for (int i = 0; i < ingredientsForPizza.Count; i++)
+            {
+                if (i % 2 == 0)
+                    Console.Write($"\n{ingredientsForPizza[i]}\t\t");
+                else
+                    Console.Write($"{ingredientsForPizza[i]}\t\t");
+            }
+        }
+    }
+}
--- Mini_Project_Assemble_Pizza/Mini_Project_Assemble_Pizza/Program.cs
+++ Mini_Project_Assemble_Pizza/Mini_Project_Assemble_Pizza/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
-
-namespace Mini_Project_Assemble_Pizza
+﻿namespace Mini_Project_Assemble_Pizza
 {
+    using Mini_Project_Assemble_Pizza.Collection;
+    using System;
+    using System.Collections.Generic;
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            OperationOfCollection operationOfCollection = new OperationOfCollection();
+            List<string> ingredientsForPizza = new List<string>();
+            operationOfCollection.FillCollection(ingredientsForPizza);
+
+
+
         }
+
+       
     }
 }
