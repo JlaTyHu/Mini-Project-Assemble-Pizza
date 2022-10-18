328e799a0cc8f0c9ed564659273d7e3191cbb321
DaraGinger <darinafrouk@gmail.com>
2022-10-18 21:29:01 +0300

    Do sorting of scores in leadboard

--- Mini_Project_Assemble_Pizza/Mini_Project_Assemble_Pizza/Leadboard/Leadboard.cs
+++ Mini_Project_Assemble_Pizza/Mini_Project_Assemble_Pizza/Leadboard/Leadboard.cs
@@ -1,38 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.IO;
-using Mini_Project_Assemble_Pizza.FieldInfoPerson;
-using Newtonsoft.Json;
-
-namespace Mini_Project_Assemble_Pizza.Leadboard
+﻿namespace Mini_Project_Assemble_Pizza
 {
-    class Leadboard
+    using System.IO;
+    using Mini_Project_Assemble_Pizza.FieldInfoPerson;
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class Leadboard
     {
-         List<InfoPerson> infoPerson = new List<InfoPerson>();
-
-        void FillingList()
+         List<InfoPerson> infoPersonList = new List<InfoPerson>();
+        InfoPerson infoPerson = new InfoPerson();
+       
+        private void FillingList()
         {
             string pathLeadboard = @"leadboard.json";
 
             using (StreamReader leadboardRead = new StreamReader(pathLeadboard))
             {
                 string jsonLeadboard = leadboardRead.ReadToEnd();
-                infoPerson = JsonConvert.DeserializeObject<List<InfoPerson>>(jsonLeadboard);
+                infoPersonList = JsonConvert.DeserializeObject<List<InfoPerson>>(jsonLeadboard);
+            }
+        }
+        private void SortingList()
+        {
+            int person;
+            int age;
+            string name;
+
+            foreach (var person1 in infoPersonList)
+            {
+                foreach (var person2 in infoPersonList)
+                {
+                    var resultOfCompare = person1.Record.CompareTo(person2.Record);
+
+                    if (resultOfCompare == 1)
+                    {
+                        person = person1.Record;
+                        person1.Record = person2.Record;
+                        person2.Record = person;
+
+                        age = person1.Age;
+                        person1.Age = person2.Age;
+                        person2.Age = age;
+
+                        name = person1.Name;
+                        person1.Name = person2.Name;
+                        person2.Name = name;
+                    }
+                }
             }
         }
 
-        void SortingList()
+        private void DisplayList()
         {
-            infoPerson = infoPerson.OrderByDescending(x => x.Record).ToList();
-        }
-
-        void DisplayList()
-        {
-            foreach (var person in infoPerson)
+            foreach (var person in infoPersonList)
             {
                 Console.WriteLine($"{person.Name} \t {person.Age} \t {person.Record}");
             }
+        }
+
+        private void AddIntoLeadboard(string name, int age, int score)
+        {
+            infoPerson.Name = name;
+            infoPerson.Age = age;
+            infoPerson.Record = score;
+            infoPersonList.Add(infoPerson);
+        }
+
+        public void MainLeadboard()
+        {
+            FillingList();
+            SortingList();
+            DisplayList();
         }
     }
 }
--- Mini_Project_Assemble_Pizza/Mini_Project_Assemble_Pizza/Lobby/Lobby.cs
+++ Mini_Project_Assemble_Pizza/Mini_Project_Assemble_Pizza/Lobby/Lobby.cs
@@ -3,11 +3,12 @@ using Mini_Project_Assemble_Pizza.FieldInfoPerson;
 
 namespace Mini_Project_Assemble_Pizza.Lobby
 {
-    internal class Lobby
+     class Lobby
     {
-         void Registration()
+        InfoPerson infoPerson = new InfoPerson();
+        void Registration()
         {
-            InfoPerson infoPerson = new InfoPerson();
+            
             Console.WriteLine("Registration");
 
             Console.WriteLine("Enter your name:");
@@ -17,36 +18,26 @@ namespace Mini_Project_Assemble_Pizza.Lobby
             Console.WriteLine("Enter your age:");
 
             infoPerson.Age = Int32.Parse(Console.ReadLine());
-
-            
         }
 
         void BeginOfGame()
         {
             Console.WriteLine("Lobby");
 
-            bool error = true;
+            Console.WriteLine("Begin game (Press y) \t Leadboeard(Press t)");
 
-            while (error)
+            char userChoice = Char.Parse(Console.ReadLine());
+            
+            if (userChoice == 'y')
             {
-                Console.WriteLine("Begin game (Press y) \t Leadboeard(Press t)");
+                
+            }
 
-                char userChoice = Char.Parse(Console.ReadLine());
-
-                switch (userChoice)
-                {
-                    case 'y':
-                        error = false;
-                        break;
-                    case 't':
-                        error = false;
-                        break;
-                    default:
-                        error = true;
-                        Console.WriteLine("No such option exist");
-                        break;
-                }
+            else if( userChoice == 't')
+            {
+                Leadboard leadboard = new Leadboard();
+                leadboard.MainLeadboard();
             }
         }
-    }
+     }
 }
